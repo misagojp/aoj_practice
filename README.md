5f94926fc3dfb96639496040ad05847e971610df
kokuto_jp <kokutojp@gmail.com>
2013-06-24 16:46:40 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+AOJ_practise-misagojp
+==================
---
+++ src/com/gmail/misagojp/aoj/itp1/n1/a/Main.java
@@ -0,0 +1,14 @@
+package com.gmail.misagojp.aoj.itp1.n1.a;
+
+/**
+ * Course ITP1 # 1 A
+ *
+ * Hello World
+ *
+ * @author misago_jp
+ */
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
