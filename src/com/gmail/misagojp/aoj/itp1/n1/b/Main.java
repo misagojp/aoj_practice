06a7e35e4c229b7745d1b5c3d1dc3e3f2d638eee
misagojp <misagojp@gmail.com>
2013-06-24 18:31:52 +0900

    Add ITP1#1-B X Cubic

---
+++ src/com/gmail/misagojp/aoj/itp1/n1/b/Main.java
@@ -0,0 +1,17 @@
+package com.gmail.misagojp.aoj.itp1.n1.b;
+
+import java.util.Scanner;
+
+/**
+ * Course ITP1 # 1 B
+ *
+ * X Cubic
+ *
+ * @author misago_jp
+ */
+public class Main {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println((int) Math.pow(s.nextInt(), 3));
+	}
+}
