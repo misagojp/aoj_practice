d83e081c1b516850f384b9f17d3897b2a5363fdd
misagojp <misagojp@gmail.com>
2013-06-25 15:42:59 +0900

    Add ITP1#3-A Print Many Hello World

---
+++ src/com/gmail/misagojp/aoj/itp1/n3/a/Main.java
@@ -0,0 +1,17 @@
+package com.gmail.misagojp.aoj.itp1.n3.a;
+
+/**
+ * Course ITP1 # 3 A
+ *
+ * Print Many Hello World
+ *
+ * @author misago_jp
+ */
+public class Main {
+	public static void main(String[] args) {
+		String h = "Hello World";
+		for (int i = 0; i < 1000; ++i) {
+			System.out.println(h);
+		}
+	}
+}
